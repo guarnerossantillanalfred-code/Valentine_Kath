c769faedc99ed83f064e1064dfd37d6e10d30038
guarnerossantillanalfred-code <guarneros.santillan.alfred@gmail.com>
2026-02-13 01:37:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "I knew you would say yes!";
 
     catImg.src = "cat_dance.gif";
 
